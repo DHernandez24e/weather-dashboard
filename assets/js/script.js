904de7be4392cb89809d40d781ad2a13866fd2ad
Doggo <dhernandez24e@hotmail.com>
2020-04-19 02:30:54 -0700

    added forecast API call, appended data to elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,8 @@ var citySearch = function () {
 
     var city = $("#city-search").val();
 
-    console.log(city);
     apiCurrentCall(city);
-    // apiForecastCall(city);
+    apiForecastCall(city);
 
     $("#city-search").val("");
     
@@ -28,23 +27,22 @@ var apiCurrentCall = function (city) {
             response.json().then(function (data) {
 
                 //Removes class "d-none" to show element
-                //$("#current-weather-container").removeClass("d-none");
+                $("#current-weather-container").removeClass("d-none");
 
                 //Grabbing weather icons and appending them via img tag
-                var iconcode = data.weather[0].icon;
-                var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png"
+                var iconCode = data.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
 
                 //Getting date from epoc time, converting to calendar date and appending to element
                 var currentDate = new Date(data.dt * 1000);
                 $("#current-date").text("(" + currentDate.toLocaleDateString() + ")");
-                console.log(currentDate);
 
                 //Appending weather data from API call to elements.
                 $("#city-name").text(data.name);
                 $("#w-icon").attr("src", iconUrl);
-                $("#current-temp").text("Temperature: " + data.main.temp);
-                $("#current-hum").text("Humidity: " + data.main.humidity);
-                $("#current-wind-sp").text("Wind Speed: " + data.wind.speed);
+                $("#current-temp").text("Temperature: " + data.main.temp + " °F");
+                $("#current-hum").text("Humidity: " + data.main.humidity + " %");
+                $("#current-wind-sp").text("Wind Speed: " + data.wind.speed + " MPH");
 
                 //Grabbing latitude and longitude of location for UV index API call
                 var lon = data.coord.lon;
@@ -70,22 +68,81 @@ var apiCurrentCall = function (city) {
     });
 }
 
-// var apiForecastCall = function (city) {
+//Forecast API call
+var apiForecastCall = function (city) {
 
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=6846f778fdbdbb000e63e44f82ef974e";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=6846f778fdbdbb000e63e44f82ef974e";
 
-//     fetch(apiUrl).then(function (response) {
-//         if (response.ok) {
-//             response.json().then(function (data) {
-//                 console.log(data);
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
 
-//                 $("#city-name-date").text(data.city.name);
+                //First Forecast Day
+                $("#fore-first-temp").text("Temp: " + data.list[1].main.temp + "°F");
+                $("#fore-first-hum").text("Humidity: " + data.list[1].main.humidity + "%");
+                //First weather icon
+                var firstIconCode = data.list[1].weather[0].icon;
+                var firstIconUrl = "http://openweathermap.org/img/w/" + firstIconCode + ".png"
+                $("#fore-first-icon").attr("src", firstIconUrl);
 
-//             })
-//         } else {
-//             alert("Error: " + response.statusText);
-//         }
-//     })
-// }
+                //Converting first date and appending to first forecast element
+                var foreDateOne = new Date(data.list[1].dt * 1000);
+                $("#fore-first-dt").text(foreDateOne.toLocaleDateString());
+
+                //Second Forecast Day
+                $("#fore-second-temp").text("Temp: " + data.list[9].main.temp + "°F");
+                $("#fore-second-hum").text("Humidity: " + data.list[9].main.humidity + "%");
+                //Second weather icon
+                var secondIconCode = data.list[9].weather[0].icon;
+                var secondIconUrl = "http://openweathermap.org/img/w/" + secondIconCode + ".png"
+                $("#fore-second-icon").attr("src", secondIconUrl);
+
+                //Converting second date and appending to second forecast element
+                var foreDateTwo = new Date(data.list[9].dt * 1000);
+                $("#fore-second-dt").text(foreDateTwo.toLocaleDateString());
+
+                //Third Forecast Day
+                $("#fore-third-temp").text("Temp: " + data.list[17].main.temp + "°F");
+                $("#fore-third-hum").text("Humidity: " + data.list[17].main.humidity + "%");
+                //Third weather icon
+                var thirdIconCode = data.list[17].weather[0].icon;
+                var thirdIconUrl = "http://openweathermap.org/img/w/" + thirdIconCode + ".png"
+                $("#fore-third-icon").attr("src", thirdIconUrl);
+
+                //Converting third date and appending to third forecast element
+                var foreDateThree = new Date(data.list[17].dt * 1000);
+                $("#fore-third-dt").text(foreDateThree.toLocaleDateString());
+
+                //Fourth Forecast Day
+                $("#fore-fourth-temp").text("Temp: " + data.list[25].main.temp + "°F");
+                $("#fore-fourth-hum").text("Humidity: " + data.list[25].main.humidity + "%");
+                //Fourth weather icon
+                var fourthIconCode = data.list[25].weather[0].icon;
+                var fourthIconUrl = "http://openweathermap.org/img/w/" + fourthIconCode + ".png"
+                $("#fore-fourth-icon").attr("src", fourthIconUrl);
+
+                //Converting fourth date and appending to fourth forecast element
+                var foreDateFour = new Date(data.list[25].dt * 1000);
+                $("#fore-fourth-dt").text(foreDateFour.toLocaleDateString());
+
+                //Fifth Forecast Day
+                // console.log(data.list[33]);
+                $("#fore-fifth-temp").text("Temp: " + data.list[33].main.temp + "°F");
+                $("#fore-fifth-hum").text("Humidity: " + data.list[33].main.humidity + "%");
+                //Fifth weather icon
+                var fifthIconCode = data.list[33].weather[0].icon;
+                var fifthIconUrl = "http://openweathermap.org/img/w/" + fifthIconCode + ".png"
+                $("#fore-fifth-icon").attr("src", fifthIconUrl);
+
+                //Converting fifth date and appending to fifth forecast element
+                var foreDateFive = new Date(data.list[33].dt * 1000);
+                $("#fore-fifth-dt").text(foreDateFive.toLocaleDateString());
+
+            })
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+}
 
 submitBtnEl.addEventListener("click", citySearch);
